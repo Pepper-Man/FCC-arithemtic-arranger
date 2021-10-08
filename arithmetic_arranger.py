a6854f08a65e15b0b229dba3f95f23ff1b888e24
Jake Boraman <jake.boraman@gmail.com>
2021-10-08 19:07:43 +0100

    Version passes all 10 tests

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -16,9 +16,11 @@ def arithmetic_arranger(problems, getAnswers=False):
     if(len(problems) > 5):
         return "Error: Too many problems."
     for problem in problems:
+        print(problem)
+        original = problem
         addIndex = problem.find('+')
         subIndex = problem.find('-')
-        quotIndex = problem.find('"')
+        quotIndex = problem.find('\'')
 
         firstStart = 0
         firstEnd = 0
@@ -30,10 +32,18 @@ def arithmetic_arranger(problems, getAnswers=False):
             firstStart = quotIndex + 1
             firstEnd = addIndex - 1
             num1 = problem[firstStart:firstEnd]
+            if(num1.isnumeric() == False):
+                return "Error: Numbers must only contain digits."
+            elif(len(num1) > 4):
+                return "Error: Numbers cannot be more than four digits."
             secondStart = addIndex + 2
             secondEnd = len(problem) - 1
             num2 = problem[secondStart:secondEnd]
-            totalWidth = len(max(num1, num2)) + 2
+            if(num2.isnumeric() == False):
+                return "Error: Numbers must only contain digits."
+            elif(len(num2) > 4):
+                return "Error: Numbers cannot be more than four digits."
+            totalWidth = len(str(max(int(num1), int(num2)))) + 2
             line1spaces = totalWidth - len(num1)
 
             # Output construction
@@ -50,7 +60,7 @@ def arithmetic_arranger(problems, getAnswers=False):
             if(problems.index(problem) != len(problems) - 1):
                 lineThree += "    "
 
-            if(getAnswers):
+            if(getAnswers == True):
                 answer = int(num1) + int(num2)
                 line4spaces = totalWidth - len(str(answer))
                 lineFour += ' ' * line4spaces
@@ -63,10 +73,18 @@ def arithmetic_arranger(problems, getAnswers=False):
             firstStart = quotIndex + 1
             firstEnd = subIndex - 1
             num1 = problem[firstStart:firstEnd]
+            if(num1.isnumeric() == False):
+                return "Error: Numbers must only contain digits."
+            elif(len(num1) > 4):
+                return "Error: Numbers cannot be more than four digits."
             secondStart = subIndex + 2
             secondEnd = len(problem) - 1
             num2 = problem[secondStart:secondEnd]
-            totalWidth = len(max(num1, num2)) + 2
+            if(num2.isnumeric() == False):
+                return "Error: Numbers must only contain digits."
+            elif(len(num2) > 4):
+                return "Error: Numbers cannot be more than four digits."
+            totalWidth = len(str(max(int(num1), int(num2)))) + 2
             line1spaces = totalWidth - len(num1)
 
             # Output construction
@@ -83,12 +101,13 @@ def arithmetic_arranger(problems, getAnswers=False):
             if(problems.index(problem) != len(problems) - 1):
                 lineThree += "    "
 
-            if(getAnswers):
+            if(getAnswers == True):
                 answer = int(num1) - int(num2)
                 line4spaces = totalWidth - len(str(answer))
                 lineFour += ' ' * line4spaces
                 lineFour += str(answer)
-                lineFour += "    "
+                if(problems.index(problem) != len(problems) - 1):
+                    lineFour += "    "
 
         else:
             return "Error: Operator must be '+' or '-'."
@@ -97,7 +116,7 @@ def arithmetic_arranger(problems, getAnswers=False):
     arranged_problems += lineTwo
     arranged_problems += "\n"
     arranged_problems += lineThree
-    if(getAnswers):
+    if(getAnswers == True):
         arranged_problems += "\n"
         arranged_problems += lineFour
     return arranged_problems
